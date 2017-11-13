f325ba41b216554c922bc434dee6a7734fc3591a
Arturo Burela <uno@arturoburela.com>
2017-11-12 20:46:40 -0600

    ok

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,5 +7,8 @@ BINDIR=$ROOTDIR/bin
 export GOPATH=$ROOTDIR
 export GOOS=linux
 
+apt-get install build-essential
+apt-get install cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
+
 go build -ldflags="-s -w" -o $BINDIR/supply nodejs/supply/cli
 go build -ldflags="-s -w" -o $BINDIR/finalize nodejs/finalize/cli
